a8c781a7472c734c2810217512b2a7ce99599d6e
ShanjinurIslam <spondoncsebuet@gmail.com>
2020-03-24 12:23:23 +0600

    Modifications

---
+++ Basic Python/list.py
@@ -0,0 +1,5 @@
+animals = ['Tiger','Lion','Elephant','Mouse']
+
+var = [61,55,66,71,11]
+
+li = [1,2,'a',True]
---
+++ Basic Python/list_methods.py
@@ -0,0 +1,22 @@
+basket = [1,2,3,4,5]
+basket.append(6)
+print(basket)
+
+basket.insert(5,2)
+print(basket)
+
+arr = [7,8,9,10]
+
+basket.extend(arr)
+
+print(basket)
+
+#removing
+
+basket.pop()
+print(basket)
+basket.remove(2)
+print(basket)
+
+basket.sort()
+print(basket)
